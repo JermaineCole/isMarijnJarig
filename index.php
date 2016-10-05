fd0bab4048ab8deb54dfd52914bdda0a2255d7d6
rensknoors <rensknoors@gmail.com>
2016-10-05 11:27:37 +0200

    images

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
-<?php include_once("analyticstracking.php") ?>
-<header></header>
-<main>
-    
+  <?php include_once("analyticstracking.php") ?>
+  <header></header>
+  <main>
+
    <nav class="orange lighten-3 " role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Marijn</a>
       <ul class="right hide-on-med-and-down">
@@ -27,14 +27,18 @@
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
   </nav>
+
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <div class="row">
         <div class="col s6 offset-s3">
           <div class="card">
+            <div class="card-image">
+              <img id="marijnPic" src="img/matrijn.jpg">
+            </div>
             <div class="card-content">
               <span class="card-title">Is Marijn jarig vandaag?</span>
-              <p class="grey-text text-lighten-1">Marijn A.K.A. Matrijn, ginger en pleb</p>
+              <p class="grey-text">Marijn A.K.A. Matrijn, ginger en pleb</p>
             </div>
             <div class="card-action">
               <a id="ja" class=""></a>
@@ -47,72 +51,73 @@
     </div>
   </div> 
 </main>
- 
-  
 
 
+
+
+<div class="container">
+</div>
+<br><br>
+
+<div class="section">
+
+</div>
+</div>
+
+<footer class="page-footer orange darken-3">
   <div class="container">
-  </div>
-    <br><br>
-
-    <div class="section">
-
+    <div class="row">
+      <div class="col l6 s12">
+        <h5 class="white-text">Dit domein is te koop. </h5>
+        <p class="grey-text text-lighten-4">(behalve voor Marijn van Rijt)</p>
+      </div>
+      <div class="col l4 offset-l2 s12">
+        <h5 class="white-text">Links</h5>
+        <ul>
+          <li><a class="grey-text text-lighten-3" href="http://idolsaudities.nl">Idolsaudities</a></li>
+          <li><a class="grey-text text-lighten-3" href="http://sonnyluu.nl/Marijn/demo.html">Marijn fansite</a></li>
+        </ul>
+      </div>
     </div>
   </div>
-
-  <footer class="page-footer orange darken-3">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Dit domein is te koop. </h5>
-                <p class="grey-text text-lighten-4">(behalve voor Marijn van Rijt)</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="http://idolsaudities.nl">idolsaudities</a></li>
-                  <li><a class="grey-text text-lighten-3" href="http://sonnyluu.nl/Marijn/demo.html">Marijn fansite</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!"></a>
-            </div>
-          </div>
-        </footer>
+  <div class="footer-copyright">
+    <div class="container">
+      © 2014 Copyright Text
+      <a class="grey-text text-lighten-4 right" href="#!"></a>
+    </div>
+  </div>
+</footer>
 
 
-  <!--  Scripts-->
+<!--  Scripts-->
 
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script>
-            var d = new Date();
-            var n = d.getDate();
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script>
+  var d = new Date();
+  var n = d.getDate();
 
-            var z = new Date();
-            var x = z.getMonth();
+  var z = new Date();
+  var x = z.getMonth();
 
 
-            console.log(n+""+x);
+  console.log(n+""+x);
 
-            if (n == 7 && x == 2)
-            {
-                $('#ja').text("Ja");
-                $('#nee').text("Zeker niet");
-                $('#nee').addClass("grey-text text-lighten-2");
-            }
-            else
-            {
-                $('#ja').text("Ja");
-                $('#ja').addClass("grey-text text-lighten-2");
-                $('#nee').text("Zeker niet");
-            };
-        </script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
+  if (n == 7 && x == 2)
+  {
+    $('#ja').text("Ja");
+    $('#nee').text("Zeker niet");
+    $('#nee').addClass("grey-text text-lighten-2");
+    $('#marijnPic').attr("src","img/hotline.jpg");
+  }
+  else
+  {
+    $('#ja').text("Ja");
+    $('#ja').addClass("grey-text text-lighten-2");
+    $('#nee').text("Zeker niet");
+  };
+</script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
 
-  </body>
+</body>
 </html>
